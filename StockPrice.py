9d2722b84364e0133da6736c954b55a424b2e44f
linhdinh0908 <83713205+linhdinh0908@users.noreply.github.com>
2022-01-18 13:24:38 -0500

    created app with streamlit

---
+++ StockPrice.py
@@ -0,0 +1,27 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock **closing price** and **volume** of Bank of Montreal
+""")
+
+# define the ticker symbol
+tickerSymbol = 'BMO'
+
+tickerData = yf.Ticker(tickerSymbol)
+
+# get the historical prices for this ticker
+tickerDf = tickerData.history(
+    period='1d', start='2010-01-17', end='2022-01-17')
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
